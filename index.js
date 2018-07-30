f4a3f2b1f313db2a5f5f93dd05732c1b59d72440
dambre <lukas.dambr@gmail.com>
2018-07-30 17:40:55 +0300

    changed response messages

--- index.js
+++ index.js
@@ -6,10 +6,13 @@ const express = require('express')
 const AWS = require('aws-sdk')
 const app = express()
 
+// Add ClientId to authenticated providers on federated idenity settings
 const poolData = {
     UserPoolId : process.env.COGNITO_USER_POOL_ID,
     ClientId : process.env.COGNITO_CLIENT_ID
 }
+
+app.use
 
 app.get('/getIdenityId/:username/:password', (req, res) => {
     const userPool = new AmazonCognitoIdentity.CognitoUserPool(poolData)
@@ -26,33 +29,46 @@ app.get('/getIdenityId/:username/:password', (req, res) => {
         Username: username,
         Pool: userPool
     })
-
-    let identityPoolId = process.env.IDENTITY_POOL_ID
-    
+ 
     cognitoUser.authenticateUser(authenticationDetails, {
         onSuccess: (result) => {
             var accessToken = result.getAccessToken().getJwtToken()
             var idToken = result.idToken.jwtToken
+            var loginProvider = process.env.COGNITO_USER_POOL_ARN
 
-            AWS.config.region = 'eu-west-1'
+            AWS.config.region = process.env.AWS_REGION
+
             AWS.config.credentials = new AWS.CognitoIdentityCredentials({
-                IdentityPoolId: identityPoolId,
-                Logins: {
-                    'cognito-idp.eu-west-1.amazonaws.com/eu-west-1_TUzcZHL99': idToken,
-                }
+                IdentityPoolId: process.env.IDENTITY_POOL_ID,
+                Logins: {loginProvider: idToken}
             })
+
             AWS.config.credentials.get(function(err){
                 if (err) {
                     console.log(err);
+                    res.json({
+                        identityId: null,
+                        message: err.message,
+                        success: false
+                    })
+                } else {
+                    res.json({
+                        identityId: AWS.config.credentials.identityId,
+                        message: err.message,
+                        success: true
+                    })
                 }
             });
         },
         onFailure: (err) => {
             console.log(err)
+            res.json({
+                identityId: null,
+                message: err.message,
+                success: false
+            })
         },
     })
-
-    res.json({ identityId: AWS.config.credentials.identityId })
 })
 
 app.listen(process.env.PORT, () => console.log('app listening on port ' + process.env.PORT))
